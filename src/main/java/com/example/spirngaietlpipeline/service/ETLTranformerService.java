e0c7c3815843d3217d3b825b2a91c4c0950a0792
Pritesh <tinumistry@gmail.com>
2024-09-19 17:22:29 +0530

    enrichment added

--- src/main/java/com/example/spirngaietlpipeline/service/ETLTranformerService.java
+++ src/main/java/com/example/spirngaietlpipeline/service/ETLTranformerService.java
@@ -3,6 +3,7 @@ package com.example.spirngaietlpipeline.service;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.transformer.KeywordMetadataEnricher;
+import org.springframework.ai.transformer.SummaryMetadataEnricher;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,8 @@ public class ETLTranformerService {
         // enrish the documents with chat model and KeyWords
 
         KeywordMetadataEnricher keywordMetadataEnricher = new KeywordMetadataEnricher(chatModel,5   );
-
+        //SummaryMetadataEnricher summaryMetadataEnricher = new SummaryMetadataEnricher(chatModel,List.of(SummaryMetadataEnricher.SummaryType.CURRENT );
+        //return summaryMetadataEnricher.apply(splitDocuments);
         return keywordMetadataEnricher.apply(splitDocuments);
     }
 }
